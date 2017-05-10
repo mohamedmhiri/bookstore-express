22b7c0e9d3a3bd2b34948a3f0aa6fbcfa90c12cc
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-05-10 22:31:59 +0100

    updating

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Get dependencies
 const express = require('express')
-const path = require('path')
 const http = require('http')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const cors = require('cors')
+/* const passport = require('passport')
+const Auth0Strategy = require('passport-auth0')*/
 mongoose.Promise = Promise
 var session = require('express-session')
 var MongoStore = require('connect-mongo')(session)
+
 
 // mongoose.connect('mongodb://localhost:27017/data2')
 const autoIncrement = require('mongoose-auto-increment')
@@ -15,39 +18,77 @@ const connection = mongoose.connect('mongodb://root:root@ds133221.mlab.com:33221
 autoIncrement.initialize(connection)
 
 const app = express()
-app.use(require('cors')())
-// Parsers for POST data
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-// Cross Origin middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+
+
 app.use(cookieParser())
 app.use(session({
   secret: 'mySecret',
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({mongooseConnection: mongoose.connection})
-  // cookie: {maxAge: 180 * 60 * 1000 }
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000, secure: false, httpOnly: true }
 }))
+/*// Configure Passport to use Auth0
+const strategy = new Auth0Strategy({
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+  return done(null, profile)
+})
 
+passport.use(strategy)
 
+// This can be used to keep a smaller payload
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser(function (user, done) {
+  done(null, user)
+})*/
+//app.use(require('cors')())
+// Parsers for POST data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// after the code that uses bodyParser and other cool stuff
+var originsWhitelist = [
+  'http://localhost:4200'//,      //this is my front-end url for development
+  // 'http://www.myproductionurl.com'
+]
+let corsOptions = {
+  origin: (origin, callback) => {
+    let isWhitelisted = originsWhitelist.indexOf(origin) !== -1
+    callback(null, isWhitelisted)
+  },
+  credentials: true
+}
+//here is the magic
+app.use(cors(corsOptions))
+// Cross Origin middleware
+app.use((req, res, next) => {
+  //res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 // Set our api routes
 // app.use('/api', api)
 app.use('/api', require('./lib/routes/book'))
 app.use('/api', require('./lib/routes/client'))
 app.use('/api', require('./lib/routes/category'))
 app.use('/api', require('./lib/routes/cart'))
+app.use('/api', require('./lib/routes/feed'))
 
 app.use((req, res, next) => {
   res.locals.session = req.session
   next()
 })
-
 
 /**
  * Get port from environment and store in Express.
